8ba5a110ef5b3dd1827e8ac124b234b44f581da6
Logan <atmosphereasmr@gmail.com>
2018-04-27 12:03:08 -0700

    fixin

--- client/src/router.js
+++ client/src/router.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Route, Switch, IndexRedirect, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './Components/Home/home.js'
 import Search from './Components/Search/search.js'
 
 export default (
   <Switch>
-    <Route exact path="/" component={() => <Redirect to="/home:access" />}/>
-    <Route path="/home:access" component={Search} />
+    <Route path="/search" exact component={Search} />
   </Switch>
 )
