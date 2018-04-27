ca9c24281dca68f9f210c03b02caf1e702b32811
Logan <atmosphereasmr@gmail.com>
2018-04-27 11:33:20 -0700

    index redirect

--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, IndexRedirect } from 'react-router-dom';
 
 import Home from './Components/Home/home.js'
 import Search from './Components/Search/search.js'
 
 export default (
   <Switch>
+    <IndexRedirect to="/search/:access" />
     <Route component={Search} path="/search/:access" exact/>
   </Switch>
 )
